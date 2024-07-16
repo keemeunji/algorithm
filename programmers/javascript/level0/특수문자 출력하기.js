de6b2b9d97381aa037bfe0f1bf3e36e43dd0bdbc
Kim Eunji <117907077+keemeunji@users.noreply.github.com>
2024-07-16 18:45:35 +0900

    Create 특수문자 출력하기.js

---
+++ programmers/javascript/level0/특수문자 출력하기.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('close', function () {
+    console.log(`!@#$%^&*(\\'"<>?:;`)  // 템플릿 리터럴 사용,  \\ -> \
+});
